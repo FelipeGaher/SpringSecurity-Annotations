3daeccaa9d242c28aaf73abe72eeb4d3a45ca7ac
NiemandZero <felipegajardo88@gmail.com>
2017-02-14 12:03:52 -0300

    security config class added ~Felipe

---
+++ src/main/java/com/felipe/springSecurityAnnotations/configuration/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package com.felipe.springSecurityAnnotations.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+	 
+}
